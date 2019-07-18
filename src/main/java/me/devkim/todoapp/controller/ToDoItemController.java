eff18596030861057d5df00d1e689bd8ec562539
kimyongjun413 <kimyongjun413@gmail.com>
2019-07-18 23:49:07 +0900

    [ADD] create, getAll method

--- src/main/java/me/devkim/todoapp/controller/ToDoItemController.java
+++ src/main/java/me/devkim/todoapp/controller/ToDoItemController.java
@@ -2,6 +2,7 @@ package me.devkim.todoapp.controller;
 
 import me.devkim.todoapp.domain.ToDoItem;
 import me.devkim.todoapp.domain.ToDoItemAdapter;
+import me.devkim.todoapp.domain.ToDoItemRequest;
 import me.devkim.todoapp.domain.ToDoItemResponse;
 import me.devkim.todoapp.services.ToDoItemService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,29 @@ public class ToDoItemController {
         ToDoItem toDoItem = null;
         try {
             toDoItem = toDoItemService.get(id);
+        } catch (final Exception e) {
+            errors.add(e.getMessage());
+        }
+        return ToDoItemAdapter.toToDoItemResponse(toDoItem, errors);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public @ResponseBody List<ToDoItemResponse> getAll() {
+        List<String> errors = new ArrayList<>();
+        List<ToDoItem> toDoItems = toDoItemService.getAll();
+        List<ToDoItemResponse> toDoItemResponses = new ArrayList<>();
+        toDoItems.stream().forEach(toDoItem -> {
+            toDoItemResponses.add(ToDoItemAdapter.toToDoItemResponse(toDoItem, errors));
+        });
+        return toDoItemResponses;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public @ResponseBody ToDoItemResponse create(@RequestBody final ToDoItemRequest toDoItemRequest) {
+        List<String> errors = new ArrayList<>();
+        ToDoItem toDoItem = ToDoItemAdapter.toToDoItem(toDoItemRequest);
+        try {
+            toDoItem = toDoItemService.create(toDoItem);
         } catch (final Exception e) {
             errors.add(e.getMessage());
         }
