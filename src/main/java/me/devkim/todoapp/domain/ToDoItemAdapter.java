0764a6722487b95eb51b2064029f8e894d74bbfe
kimyongjun413 <kimyongjun413@gmail.com>
2019-07-18 23:51:51 +0900

    [UPDATE] ToDoItemAdapter method(toToDoItem, toToDoItemResponse)

--- src/main/java/me/devkim/todoapp/domain/ToDoItemAdapter.java
+++ src/main/java/me/devkim/todoapp/domain/ToDoItemAdapter.java
@@ -1,14 +1,25 @@
 package me.devkim.todoapp.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class ToDoItemAdapter {
 
+    public static ToDoItem toToDoItem(final ToDoItemRequest toDoItemRequest) {
+        if(toDoItemRequest == null) {
+            return null;
+        }
+        return ToDoItem.builder()
+                .title(toDoItemRequest.getTitle())
+                .done(toDoItemRequest.isDone())
+                .build();
+    }
+
     public static ToDoItemResponse toToDoItemResponse(final ToDoItem toDoItem, final List<String> errors) {
-        return ToDoItemResponse
-                .builder()
+        return ToDoItemResponse.builder()
                 .toDoItem(toDoItem)
-                .errors(errors)
+                .errors(Optional.ofNullable(errors).orElse(new ArrayList<>()))
                 .build();
     }
 }
